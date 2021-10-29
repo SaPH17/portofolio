a60222a6424b2716fe7317c5916d4b8ac0ef1000
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2021-10-29 13:36:52 +0700

    create project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Banner from './components/banner/Banner';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div>
+            TESTING
+        </div>
+    )
+}
+
+export default Banner
