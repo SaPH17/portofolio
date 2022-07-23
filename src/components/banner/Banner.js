3d0dd529803a72732a40c9030b58aa0964c50e0e
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2022-07-23 16:11:25 +0700

    refactored sass

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import styles from './Banner.module.sass';
+import React from "react"
+import styles from "./Banner.module.sass"
 
 const Banner = () => {
-
-    return (
-        <div className={styles.bannerContainer}>
-            <div className={styles.subtitle}>
-                Hi, my name is
-            </div>
-            <div className={styles.title}>
-                Stephanus Aditya
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.bannerContainer}>
+			<div className={styles.subtitle}>Hi, my name is</div>
+			<div className={`${styles.title} ${styles.typewriterAnimation}`}>
+				Stephanus Aditya
+			</div>
+			<div className={styles.titleResponsiveContainer}>
+				<div
+					className={`${styles.titleResponsive} ${styles.titleResponsiveTop}`}
+				>
+					Stephanus
+				</div>
+				<div
+					className={`${styles.titleResponsive} ${styles.titleResponsiveBot}`}
+				>
+					Aditya
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Banner
