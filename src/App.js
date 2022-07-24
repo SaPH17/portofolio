25eafed51e175ddc7ddc460cabf2515418e87808
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2022-07-24 22:51:48 +0700

    Added instagram contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Banner from "./components/banner/Banner"
 import Header from "./components/header/Header"
 import "./App.css"
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaEnvelope, FaInstagram } from "react-icons/fa"
 
 function App() {
 	const contacts = [
+		{
+			icon: <FaInstagram />,
+			url: "https://www.instagram.com/stephanus_aditya/",
+		},
 		{ icon: <FaGithub />, url: "https://github.com/SaPH17" },
 		{
 			icon: <FaLinkedin />,
